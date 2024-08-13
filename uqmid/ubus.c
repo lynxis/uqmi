8fd2148a20ac94593197408afe44f9b3b3cd31b7
Alexander Couzens <lynxis@fe80.eu>
2024-08-13 13:09:20 +0200

    uqmid: add support to set username, password for PDN
    Some APNs require to set a username, password in the wds profile.

--- uqmid/ubus.c
+++ uqmid/ubus.c
@@ -253,6 +253,8 @@ enum {
 	CFG_APN,
 	CFG_PIN,
 	CFG_ROAMING,
+	CFG_USERNAME,
+	CFG_PASSWORD,
 	__CFG_MAX
 };
 
@@ -261,6 +263,8 @@ static const struct blobmsg_policy modem_configure_policy[__CFG_MAX] = {
     [CFG_APN] = { .name = "apn", .type = BLOBMSG_TYPE_STRING },
     [CFG_PIN] = { .name = "pin", .type = BLOBMSG_TYPE_STRING },
     [CFG_ROAMING] = { .name = "roaming", .type = BLOBMSG_TYPE_BOOL },
+    [CFG_USERNAME] = { .name = "username", .type = BLOBMSG_TYPE_STRING },
+    [CFG_PASSWORD] = { .name = "password", .type = BLOBMSG_TYPE_STRING },
     };
 
 static int
@@ -270,6 +274,7 @@ modem_configure(struct ubus_context *ctx, struct ubus_object *obj,
 {
 	struct modem *modem = container_of(obj, struct modem, ubus);
 	struct blob_attr *tb[__CFG_MAX];
+	char *value;
 	int ret;
 
 	/* prevent mixing previous configure calls */
@@ -291,6 +296,20 @@ modem_configure(struct ubus_context *ctx, struct ubus_object *obj,
 
 	if (tb[CFG_ROAMING]) {
 		modem->config.roaming = blobmsg_get_bool(tb[CFG_ROAMING]);
+	}
+
+	if (tb[CFG_USERNAME]) {
+		TALLOC_FREE(modem->config.username);
+		value = blobmsg_get_string(tb[CFG_USERNAME]);
+		if (value && strlen(value))
+			modem->config.username = talloc_strdup(modem, blobmsg_get_string(tb[CFG_USERNAME]));
+	}
+
+	if (tb[CFG_PASSWORD]) {
+		TALLOC_FREE(modem->config.password);
+		value = blobmsg_get_string(tb[CFG_PASSWORD]);
+		if (value && strlen(value))
+			modem->config.password = talloc_strdup(modem, blobmsg_get_string(tb[CFG_PASSWORD]));
 	}
 
 	modem->config.pdp_type = QMI_WDS_PDP_TYPE_IPV4;
