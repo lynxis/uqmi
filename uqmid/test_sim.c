97a92c3cbea59e4bc92e6c97e6e3ce06488d597a
Alexander Couzens <lynxis@fe80.eu>
2024-08-22 02:08:14 +0200

    uqmid: move usim related function into own fsm

--- uqmid/test_sim.c
+++ uqmid/test_sim.c
@@ -9,7 +9,7 @@ int main() {
 	int ret;
 	uint8_t imsi_ef[] = { 0x08, 0x29, 0x82, 0x60, 0x82, 0x00, 0x00, 0x20, 0x80 };
 	char imsi_str[16];
-	ret = uim_decode_imsi(&imsi_ef[0], 9, &imsi_str[0], sizeof(imsi_str));
+	ret = uqmi_sim_decode_imsi(&imsi_ef[0], 9, &imsi_str[0], sizeof(imsi_str));
 	if (ret >= 0)
 		fprintf(stderr, "Decoded imsi %s\n", imsi_str);
 	assert(ret >= 0);
