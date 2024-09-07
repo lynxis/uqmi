8897d87b56af01380caeaf2fd91cea36a4b511e9
Alexander Couzens <lynxis@fe80.eu>
2024-09-07 19:52:42 +0200

    uqmid: netifd hookscript: make the hook work

--- uqmid/uqmid.sh
+++ uqmid/uqmid.sh
@@ -6,6 +6,10 @@
 	init_proto "$@"
 }
 
+_qdebug() {
+	logger -t qmi "$@"
+}
+
 PROTO_OPTIONS="apn username password pin roaming"
 proto_qmid_init_config() {
 	available=1
@@ -47,6 +51,62 @@ _qmi_ensure_device_present() {
 	_qmi_device_present "$name"
 }
 
+_qmi_poll_state() {
+	local name=$1
+	local timeout=$2
+
+	# in theory we should have multiple timeouts
+	# we should only have a long timeout if we reach netsearch
+	for _i in $(seq 1 "$timeout"); do
+		json_init
+		json_load "$(ubus call "uqmid.modem.$name" dump)"
+		json_get_var state_name state_name
+		case "$state_name" in
+		FAILED)
+			false
+			return
+			;;
+		LIVE)
+			true
+			return
+			;;
+		*)
+			# unknown state
+			sleep 1
+			;;
+		esac
+	done
+}
+
+_qmi_poll_sim_state() {
+	local name=$1
+	local timeout=$2
+
+	for _i in $(seq 1 "$timeout"); do
+		json_init
+		json_load "$(ubus call "uqmid.modem.$name" dump)"
+		json_get_var sim_state_name sim_state_name
+		case "$sim_state_name" in
+		IDLE|"WAIT UIM PRESENT"|GET_INFO)
+			# normal wait
+			sleep 1
+			;;
+		READY)
+			true
+			return
+			;;
+		CHV_PIN|CHV_PUK|FAILED)
+			false
+			return
+			;;
+		*)
+			# unknown state
+			sleep 1
+			;;
+		esac
+	done
+}
+
 proto_qmid_setup() {
 	local interface="$1"
 	local device apn 
@@ -63,10 +123,11 @@ proto_qmid_setup() {
 
 	[ -n "$ctl_device" ] && device=$ctl_device
 
+	_qdebug "Setting up interface $interface"
+
 	[ -n "$device" ] || {
-		echo "No control device specified"
+		_qdebug "No control device specified"
 		proto_notify_error "$interface" NO_DEVICE
-		proto_set_available "$interface" 0
 		return 1
 	}
 
@@ -74,9 +135,8 @@ proto_qmid_setup() {
 
 	device="$(readlink -f "$device")"
 	[ -c "$device" ] || {
-		echo "The specified control device does not exist"
+		_qdebug "The specified control device does not exist"
 		proto_notify_error "$interface" NO_DEVICE
-		proto_set_available "$interface" 0
 		return 1
 	}
 
@@ -84,9 +144,8 @@ proto_qmid_setup() {
 	devpath="$(readlink -f "/sys/class/usbmisc/$devname/device/")"
 	ifname="$( ls "$devpath"/net )"
 	[ -n "$ifname" ] || {
-		echo "The interface could not be found."
+		_qdebug "The interface could not be found."
 		proto_notify_error "$interface" NO_IFACE
-		proto_set_available "$interface" 0
 		return 1
 	}
 
@@ -94,76 +153,90 @@ proto_qmid_setup() {
 	[ -z "$name" ] && name=$(_qmid_convert_devtoname "$device")
 
 	if [ -z "$name" ]; then
-		echo "Name not set and can't derived from device $device."
+		_qdebug "Name not set and can't derived from device $device."
 		proto_notify_error "$interface" NO_NAME
-		proto_set_available "$interface" 0
 		return 1
 	fi
 
 	if ! _qmi_ensure_device_present "$name" "$device"; then
 		# can't create a device
+		_qdebug "Can't ensure the device"
 		proto_notify_error "$interface" NO_IFACE_CREATABLE
-		proto_set_available "$interface" 0
 		return 1
 	fi
 
 	# pass configuration to it
 	ubus call "uqmid.modem.$name" configure "{'apn':'$apn', 'username': '$username', 'password': '$password', 'pin': '$pin', 'roaming':'$roaming'}"
 
-	# TODO: poll every second until sim is ready for max 10 seconds
-	# fail with no modem availabil
-	sleep 10
+	_qmi_poll_sim_state "$name" 30
 
 	# check if simcard is fine
+	json_init
 	json_load "$(ubus call "uqmid.modem.$name" dump)"
-	json_get_var state
-	json_get_var simstate
+	json_get_var state_name state_name
+	json_get_var sim_state_name sim_state_name
 	# use simstate as human readable to have more stable "api"
 
-	case "$simstate" in
-		1)
-			# TODO add support for pincode/unlock
-			proto_notify_error "$interface" SIM_PIN_REQUIRED
-			proto_set_available "$interface" 0
+	case "$sim_state_name" in
+		IDLE | "WAIT UIM PRESENT" | GET_INFO)
+			_qdebug "SIM is still not ready after 30 seconds. Failing!"
+			proto_notify_error "$interface" SIM_NOT_READY
 			return 1
 			;;
-		2)
+		CHV_PIN)
+			# TODO add support for pincode/unlock
+			_qdebug "SIM REQUIRED PIN! Failing!"
+			proto_notify_error "$interface" SIM_PIN_REQUIRED
+			return 1
+			;;
+		CHV_PUK)
+			_qdebug "SIM REQUIRED PUK! Failing!"
 			proto_notify_error "$interface" SIM_PUK_REQUIRED
 			proto_set_available "$interface" 0
 			return 1
 			;;
-		3)
-			# ready state
+		READY)
+			# continue to process
+			_qdebug "SIM is READY"
 			;;
-		4)
+		FAILED)
+			_qdebug "SIM is in FAILED state. Failing!"
 			# blocked state
-			proto_notify_error "$interface" SIM_BLOCKED
-			proto_set_available "$interface" 0
+			proto_notify_error "$interface" SIM_FAILED
 			return 1
 			;;
 		*)
+			_qdebug "UNKNOWN STATE $sim_state_name"
+			ubus call "uqmid.modem.$name" dump >> /tmp/qmi.log
 			# unknown sim state
 			proto_notify_error "$interface" SIM_STATE_UNKNOWN
-			proto_set_available "$interface" 0
 			return 1
 			;;
 	esac
 
 	# poll here again for a state
 	# TODO: until it reaches LIVE or the FSM terminates in a failure mode
+	_qmi_poll_state "$name" 30
+	
+	json_init
+	json_load "$(ubus call "uqmid.modem.$name" dump)"
+	json_get_var state_name state_name
+	case "$state_name" in
+		LIVE)
+			# found our correct state
+			;;
+		*)
+			_qdebug "Modem $interface didn't entered LIVE. Instead modem is in state $state_name"
+			proto_notify_error "$interface" NOT_READY_YET
+			return 1
+			;;
+	esac
 
-	# 12 => LIVE state
-	if [ "$state" != "12" ] ; then
-		proto_notify_error "$interface" NOT_READY_YET
-		proto_set_available "$interface" 0
-		return 1
-	fi
-
-	json_get_var ipv4_addr
-	json_get_var ipv4_netmask
-	json_get_var ipv4_gateway
-	json_get_var dns1
-	json_get_var dns2
+	json_get_var ipv4_addr ipv4_addr
+	json_get_var ipv4_netmask ipv4_netmask
+	json_get_var ipv4_gateway ipv4_gateway
+	json_get_var dns1 dns1
+	json_get_var dns2 dns2
 
 	proto_init_update "$ifname" 1
 	proto_set_keep 1
@@ -262,7 +335,6 @@ proto_qmid_teardown() {
 
 	echo "Stopping network $interface"
 
-	# TODO: use stop instead of remove
 	ubus call uqmid remove_modem "{ 'name': '$name' }"
 
 	proto_init_update "*" 0
